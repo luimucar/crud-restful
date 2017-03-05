6a040f9706d0d2e737555cedb0f54848c238bec2
Claudio Margulhano <claudio.margulhano@jetsoft.com.br>
2017-03-04 23:17:18 -0300

    Initial version

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./src/decorators"));
-__export(require("./src/crud.module"));
-__export(require("./src/crud.component"));
-//# sourceMappingURL=index.js.map
